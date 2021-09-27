192a68859d2602848373dec146a12185471190ad
tuongduymanh <42239117+tuongduymanh@users.noreply.github.com>
2021-09-27 17:32:21 +0700

    manhtd commit

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://1602c818.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
